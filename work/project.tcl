26027c4a48dbc9cd3760f35e63359d1fa0e62743
unknown <davisczh@gmail.com>
2023-04-12 20:14:16 +0800

    'rom_halfdone'

--- work/project.tcl
+++ work/project.tcl
@@ -1,13 +1,13 @@
-set projDir "C:/Users/user/Documents/SUTD/term4/test_M4/work/vivado"
-set projName "test_M4"
+set projDir "C:/Users/user/Documents/SUTD/term4/now_rom/work/vivado"
+set projName "now_rom"
 set topName top
 set device xc7a35tftg256-1
 if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
 create_project $projName "$projDir/$projName" -part $device
 set_property design_mode RTL [get_filesets sources_1]
-set verilogSources [list "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/au_top_0.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/hanoi_display_1.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/reset_conditioner_2.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/dual_matrix_3.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/player_display_4.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/enemy_display_5.v" "C:/Users/user/Documents/SUTD/term4/test_M4/work/verilog/led_writer_6.v" ]
+set verilogSources [list "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/au_top_0.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/hanoi_display_1.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/rom_ish_2.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/reset_conditioner_3.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/dual_matrix_4.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/player_display_5.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/enemy_display_6.v" "C:/Users/user/Documents/SUTD/term4/now_rom/work/verilog/led_writer_7.v" ]
 import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
-set xdcSources [list "C:/Users/user/Documents/SUTD/term4/test_M4/work/constraint/custom.xdc" ]
+set xdcSources [list "C:/Users/user/Documents/SUTD/term4/now_rom/work/constraint/custom.xdc" ]
 read_xdc $xdcSources
 set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
 update_compile_order -fileset sources_1
